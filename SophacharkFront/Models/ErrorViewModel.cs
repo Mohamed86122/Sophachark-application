a04cab524d3ccf8857d0cfa1c624d7a800aba6f2
Mohamed Sghir <med.sg125@gmail.com>
2023-12-24 10:55:40 +0100

    Ajoutez des fichiers projet.

---
+++ SophacharkFront/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SophacharkFront.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
